a940827f7b4545c9039949e8ec37b0fd95eae1f7
Davide Bellettini <davide@bellettini.me>
2014-05-26 12:44:11 +0200

    Unpacking zip in package

--- vagrant/puppet/modules/jacorb/manifests/package.pp
+++ vagrant/puppet/modules/jacorb/manifests/package.pp
@@ -1,11 +1,27 @@
 class jacorb::package
+(
+  $version = '2.2.4'
+)
 {
+  $dir = "/opt/JacORB-$version"
+  $zipfile = "JacORB-$version-binary.zip"
+
   include wget
 
-  wget::fetch { "jacorb-binary":
-    source      => 'http://www.jacorb.org/releases/2.2.4/JacORB-2.2.4-binary.zip',
-    destination => '/home/vagrant/JacORB-2.2.4-binary.zip',
+  wget::fetch { 'jacorb-binary':
+    source      => "http://www.jacorb.org/releases/2.2.4/$zipfile",
+    destination => "/opt/$zipfile",
     timeout     => 0,
     verbose     => false,
+  } ->
+  exec { 'extract':
+    path => '/bin:/usr/bin',
+    cwd => '/opt/',
+    command => "unzip $zipfile",
+    creates => $dir,
+  } ->
+  file { '/opt/jacorb':
+    ensure => symlink,
+    target => $dir,
   }
 }
