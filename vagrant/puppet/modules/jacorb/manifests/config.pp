dd2a575b7e331f8fa1050fdbbfc6b6558a10ebfb
Davide Bellettini <davide@bellettini.me>
2014-05-27 11:21:51 +0200

    Copying orb.properties to /usr/lib/jvm/java-7-openjdk-amd64/lib

--- vagrant/puppet/modules/jacorb/manifests/config.pp
+++ vagrant/puppet/modules/jacorb/manifests/config.pp
@@ -1,13 +1,19 @@
 class jacorb::config
 {
+  $orbProperties = '/opt/jacorb/etc/orb.properties'
+
   augeas { 'orb.properties':
     lens => 'Properties.lns',
-    incl => '/opt/jacorb/etc/orb.properties',
+    incl => $orbProperties,
     changes => [
       'set ORBInitRef.NameService /opt/jacorb/NS_Ref',
       'set jacorb.naming.ior_filename /opt/jacorb/NS_Ref',
       'set jacorb.imr.ior_file /opt/jacorb/ImR_Ref',
     ],
+  } ->
+  file { '/usr/lib/jvm/java-7-openjdk-amd64/lib/orb.properties':
+    ensure => present,
+    source => $orbProperties,
   }
 
   $jacorbProperties = '/opt/jacorb/etc/jacorb.properties'
