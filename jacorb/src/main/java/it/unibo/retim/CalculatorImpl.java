f9a93e9eb94a927dd52b318f908bca0f825847b8
Davide Bellettini <davide@bellettini.me>
2014-05-27 14:49:35 +0200

    Implementing Calculator

---
+++ jacorb/src/main/java/it/unibo/retim/CalculatorImpl.java
@@ -0,0 +1,10 @@
+package it.unibo.retim;
+
+import retim.CalculatorPOA;
+
+public class CalculatorImpl extends CalculatorPOA {
+    @Override
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
