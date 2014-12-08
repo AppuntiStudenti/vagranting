ad1a26d2b439a6e58d92262eceb2f12a52b21e9a
Davide Bellettini <davide.bellettini@studio.unibo.it>
2014-12-08 16:41:06 +0100

    Small puppet fix

--- vagrant/puppet/modules/java/manifests/init.pp
+++ vagrant/puppet/modules/java/manifests/init.pp
@@ -3,14 +3,14 @@ class java
   include apt
 
   apt::ppa { 'ppa:webupd8team/java': } ->
-  exec { 'accept-license':
+  exec { 'accept-license-v8':
     path => '/bin:/usr/bin',
     command => 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections',
   } ->
   package { 'oracle-java8-installer':
     ensure => present
   } ->
-  exec { 'accept-license':
+  exec { 'accept-license-v7':
     path => '/bin:/usr/bin',
     command => 'echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections',
   } ->
